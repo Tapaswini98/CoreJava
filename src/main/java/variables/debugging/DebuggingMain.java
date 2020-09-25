cf23a9b76353770137e3ca42055c7c5238649ad3
Tapaswini kar <kar.tapaswini1111@gmail.com>
2020-09-25 09:02:34 +0530

    Local and Clas variables added

---
+++ src/main/java/variables/debugging/DebuggingMain.java
@@ -0,0 +1,18 @@
+package variables.debugging;
+
+public class DebuggingMain {
+    static int staticValue = 10;
+    int nonstaticValue= 200;
+
+    public static void main(String[] args) {
+        DebuggingMain d = new DebuggingMain();
+        d.sum(5,6);
+    }
+  public  DebuggingMain(){
+      System.out.println("inside constructor");
+  }
+    public void sum(int x , int y){
+        int result = x + y;
+        System.out.println("sum method");
+    }
+}
